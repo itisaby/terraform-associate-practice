54ea19dbf0694dc2123836e2dd5fbf59ff3091a2
itisaby <arnabbibhuti01@gmail.com>
2022-08-26 16:48:06 +0530

    gcp config

--- Providers-GCP/gcp.tf
+++ Providers-GCP/gcp.tf
@@ -1,1 +1,28 @@
-    
+    provider "google" {
+  project = "{{YOUR GCP PROJECT}}"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
